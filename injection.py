41b20783a76c6ebf3336d40e3caaa2ae333e06a1
Diego Cardoso Nunes <diegocar.nunes@gmail.com>
2021-04-13 23:55:12 -0300

    Support for environment based dependecy injection

--- injection.py
+++ injection.py
@@ -1,12 +1,14 @@
 import inject
+import os
 
 from tickets.adapters.storage.ticket_repository import InMemoryTicketRepository
 from tickets.adapters.storage.account_repository import InMemoryAccountRepository
 from tickets.application.ports import TicketRepository, AccountRepository
 
 
-def configure_injection(env="dev"):
+def configure_injection():
     def configuration(binder: inject.Binder):
+        env = os.environ.get('ENV') or "dev"
         if env == "dev":
             binder.bind(TicketRepository, InMemoryTicketRepository())
             binder.bind(AccountRepository, InMemoryAccountRepository())
